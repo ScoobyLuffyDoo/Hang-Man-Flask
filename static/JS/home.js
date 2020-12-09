5b2822b0dc2429507c95b76903397e9ee93396bb
ScoobyLuffyDoo <you@example.com>
2020-12-10 00:47:39 +0200

    gallows Design

--- static/JS/home.js
+++ static/JS/home.js
@@ -1,12 +1,32 @@
 window.onload = function(){
 var i =0,text;
-text ="Welcome \n To\n Hang Man"
+text ="Hang Man"
 function typing(){
     if(i<text.length){
         document.getElementById("GameTitle").innerHTML += text.charAt(i);
         i++;
-        setTimeout(typing,50)
+                setTimeout(typing,100)
     }
 }
+
+function drwaHangMangallows(){
+    var c = document.getElementById("HangMan");
+    var ctx = c.getContext("2d");
+    var ctx2 = c.getContext("2d");
+    ctx2.lineWidth = 3;
+    ctx2.beginPath();
+    ctx2.moveTo(180,290);
+    ctx2.lineTo(180,3);
+    ctx2.moveTo(182,4);
+    ctx2.lineTo(100,4);
+    ctx2.lineTo(100,40);
+    ctx2.moveTo(120,40);
+    ctx2.lineTo(80,40);
+    ctx2.moveTo(200,149);
+    ctx2.lineTo(160,149);
+    ctx2.stroke()    
+    
+}
 typing();
+drwaHangMangallows();
 }
